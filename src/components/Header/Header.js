8f8643bcc9a25cd08b16004495d1cd0fe5cd4859
MaxHord <max981hord@gmail.com>
2021-03-03 00:23:30 +0200

    Add prodject

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+import Logo from './logo.png';
+
+export default function Header() {
+    return (
+        <header>
+          <div class="container center-nav">
+            <nav class=" navbar navbar-expand-lg  ">
+              <a href="#" class="navbar-brand">
+                <img src={Logo} alt="logo" width="64px" height="64px" />
+              </a>
+            </nav>
+          </div>
+        </header>
+    )
+}
